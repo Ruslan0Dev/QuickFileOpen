7b2790811f63c219feb9c4e7ecddde4e63ced844
nfs911 <nfs911@gmail.com>
2017-11-18 18:57:13 +0200

    Added ability to set title

--- QuickFileOpen.py
+++ QuickFileOpen.py
@@ -1,59 +1,27 @@
 import sublime
 import sublime_plugin
-import sys, os
+import sublime_api
 
 class QuickFileOpenCommand(sublime_plugin.WindowCommand):
 
-  SETTING_PATH = 'QuickFileOpen.sublime-settings'
-  data = []
+    def run(self):
+        settings = sublime.load_settings('QuickFileOpen.sublime-settings')
+        index = '0'
+        titles = []
+        while settings.has(index):
+            titles += [settings.get(index)[0]]
+            index = str(int(index) + 1)
+        if type(titles) == list:
+            self.window.show_quick_panel(titles, self.on_done)
+        elif item is None:
+            self.window.show_quick_panel(['Set the \'items\' setting to use QuickFileOpen'], None)
+        else:
+            sublime.error_message('The \'items\' setting must be a list')
 
-  def run(self):
-    self.data = sublime.load_settings(self.SETTING_PATH).get('item')
-    items = self._get_content(self.data)
-
-    self.show_panel(items)
-
-  #  get one hierarchy item
-  def _get_content(self, items):
-    ret = []
-    for item in items:
-
-      if type(item['path']) == list:
-        name = "[D] " + item['name']
-        tmp = [ x['name'] for x in item['path'] ]
-        path = ", ".join(tmp)
-      else:
-        name = item['name']
-        path = item['path']
-
-      ret.append([ name, path ])
-    return ret
-
-  def show_panel(self, items):
-
-    if type(items) == list:
-      self.window.show_quick_panel(items, self.on_done)
-    elif items is None:
-      self.window.show_quick_panel(["Set the 'files' setting to use QuickFileOpen"], None)
-    else:
-      sublime.error_message("The 'files' setting must be a list")
-
-
-  def on_done(self, idx):
-    if idx == -1:
-      return
-
-    # get recursive contents
-    if type(self.data[idx]['path']) == list:
-
-      self.data = self.data[idx]['path']
-      items = self._get_content(self.data)
-      self.show_panel(items)
-    
-    # get normal path
-    else:
-      filepath = self.data[idx]['path']
-      if not os.path.exists(filepath):
-        return
-
-      self.window.open_file(filepath)
+    def on_done(self, selected):
+        if selected == -1:
+            return
+        settings = sublime.load_settings('QuickFileOpen.sublime-settings')
+        file = settings.get(str(selected))[1]
+        if(file != ''):
+            self.window.open_file(file)
